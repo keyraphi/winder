9fc72a32c3311e00f37b326f121540110fe0a513
keyraphi <keyraphi@gmail.com>
2026-06-09 22:42:02 +0200

    Make comparison vidos slower and more detailed by default

--- examples/compare_mesh.py
+++ examples/compare_mesh.py
@@ -405,7 +405,7 @@ def main():
     parser.add_argument(
         "--frames_per_sequence",
         type=positive_type,
-        default=100,
+        default=200,
         help="Frames per shift sweep.",
     )
     parser.add_argument(
--- examples/compare_points.py
+++ examples/compare_points.py
@@ -518,7 +518,7 @@ def main():
     parser.add_argument(
         "--frames_per_sequence",
         type=positive_type,
-        default=100,
+        default=200,
         help="Frames per shift sweep.",
     )
     parser.add_argument(
